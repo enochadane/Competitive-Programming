f0485b724f49c83691b9733d55b67db6f5e62edc
Henok A <henokadane8@gmail.com>
2021-01-04 08:44:29 +0300

    two sum

---
+++ week-1/twoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let indices = [];
+    for(let i = 0; i <= nums.length-1; i++) {
+        for(let j = i  + 1; j <= nums.length - 1; j++) {
+            if(nums[i] + nums[j] == target) {
+                indices.push(i);
+                indices.push(j);
+                // return;
+            }
+        }
+    }
+    return indices;
+};
