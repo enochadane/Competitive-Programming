24115f7d4f83325c02e6238495ee50b00277898e
Henok A <henokadane8@gmail.com>
2021-01-06 15:28:07 +0300

    reverse integer

---
+++ week-1/reverse.py
@@ -0,0 +1,23 @@
+def reverse(x):
+    x = str(x)
+    y = list(x)
+    z = []
+    reversedInt = ''
+    lowerEnd = -1
+    firstNonZeroIndex = 0
+    if(y[0] == '-'):
+        reversedInt = '-'
+        lowerEnd = 0
+    for i in range(len(y) - 1, lowerEnd, -1):
+        z.append(y[i])
+    for i in range(len(z)):
+        if z[i] != '0':
+            firstNonZeroIndex = i
+            break
+    reversedInt = reversedInt + ''.join(z[firstNonZeroIndex:])
+    print(reversedInt)
+    if(int(reversedInt) < -2**31 or int(reversedInt) > (2**31) - 1):
+        return 0
+    return reversedInt
+
+reverse(2147483649)
