e03966ed699154ba35db4ef0bef20f8a0a382561
Henok A <henokadane8@gmail.com>
2021-01-08 22:13:23 +0300

    mark and toys

--- week-1/markAndToys.py
+++ week-1/markAndToys.py
@@ -1,22 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
 def maximumToys(prices, k):
     total = 0
     maxToys = 0
-    maxPrice = 0
-    sortedPrices = []
-    for i in range(len(prices)):
-        if prices[i] > maxPrice:
-            maxPrice = prices[i]
-    countArray = [0] * (maxPrice + 1)
-    for i in range(len(prices)):
-        countArray[prices[i]] += 1
-    for i in range(len(countArray)):
-        for j in range(countArray[i]):
-            sortedPrices.append(i)
+    sortedPrices = sorted(prices)
     i = 0
     while(i < len(sortedPrices) and total + sortedPrices[i] <= k):
         total += sortedPrices[i]
         maxToys += 1
         i += 1
-    print(maxToys)
     return maxToys
-maximumToys([1, 12, 5, 111, 200, 1000, 10], 50)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
