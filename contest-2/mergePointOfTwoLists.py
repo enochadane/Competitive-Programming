9f2ee0dab949133824dbf61edd6e63e654bd1dcd
enochadane <henokadane8@gmail.com>
2021-01-16 23:46:46 +0300

    using stacks

--- contest-2/mergePointOfTwoLists.py
+++ contest-2/mergePointOfTwoLists.py
@@ -20,3 +20,32 @@ def findMergeNode(head1, head2):
         start = head2
         
         
+
+
+# Complete the findMergeNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def findMergeNode(head1, head2):
+    stack1 = []
+    stack2 = []
+    mergePoint = None
+    while head1 is not None:
+        stack1.append(head1)
+        head1 = head1.next
+    while head2 is not None:
+        stack2.append(head2)
+        head2 = head2.next
+    for i in range(len(stack1)):
+        if len(stack2) != 0 and stack1[len(stack1) -1 - i] == stack2[len(stack2) -1 - i]:
+            mergePoint = stack1[len(stack1) - 1 - i].data
+        else:
+            break
+    return mergePoint
+            
