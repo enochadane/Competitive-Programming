13fe95671e638e1754da23a988154c3fefcd539b
enochadane <henokadane8@gmail.com>
2021-01-16 23:47:16 +0300

    recursive degit sum

--- week-3/recursiveDigitSum.py
+++ week-3/recursiveDigitSum.py
@@ -1,5 +1,33 @@
-p = "sdkfjdl"
-# p = 123
-print(int(list(p)))
+#!/bin/python3
 
-print(sum([2, 3]))
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the superDigit function below.
+def superDigit(n, k):
+    total = 0
+    if len(n) <= 1:
+        return int(n)
+    
+    for i in range(len(n)):
+        total += int(n[i])
+    return superDigit(str(total) * k, 1)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = nk[0]
+
+    k = int(nk[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
