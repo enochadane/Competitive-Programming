012fbeab400371956bfb5ae57b466fe0e5f16279
enochadane <henokadane8@gmail.com>
2021-03-19 00:54:50 +0300

    contest problems

--- week-10 camp/ambiguous_coordinates.py
+++ week-10 camp/ambiguous_coordinates.py
@@ -1,20 +1,49 @@
-a = ['1','2','33','44']
-
-for i in range(len(a)):
-    a[i] = a[i] + '..'
-
-b = '0'
-c = [d for d in b]
-print(len(c) > 0)
-
-for i in range(1, len(b)):
-    x = b[:i]
-    y = b[i:]
-    print(x, y)
-
-print([1,2,3] + ['k', 'e'])
-
-print(float('7'))
-
-
-print(float(1e-05))
+class Solution:
+    def ambiguousCoordinates(self, S: str) -> List[str]:
+        validStrings = []
+        s = S[1:len(S) -1]
+        for i in range(1, len(s)):
+            x = s[:i]
+            y = s[i:]
+            
+            x_possibilities = [x]
+            y_possibilities = [y]
+            if len(x) > 1:
+                x_possibilities += self.insertDecimal(x)
+                
+            if len(y) > 1:
+                y_possibilities += self.insertDecimal(y)
+            
+            
+            for x_num in x_possibilities:
+                for y_num in y_possibilities:
+                    if self.isValidString(x_num) and self.isValidString(y_num):
+                        coordinate = '(' + x_num + ', ' + y_num + ')'
+                        validStrings.append(coordinate)
+            
+        return validStrings
+    
+    def insertDecimal(self, num: str) -> List[str]:
+        possibleStrings = []
+        num = [digit for digit in num]
+        for i in range(len(num) -1):
+            decimal = num.copy()
+            decimal[i] = decimal[i] + '.'
+            possibleStrings.append(''.join(decimal))
+        
+        return possibleStrings
+    
+    def isValidString(self, num: str) -> bool:
+        num = [digit for digit in num]
+        if len(num) > 1 and num[0] == '0' and num[len(num) -1] == '0':
+            return False
+        if len(num) > 1 and num[0] == '0' and num[1] != '.':
+            return False
+        if num[len(num) -1] == '0' and '.' in num:
+            return False
+        
+        return True
+    
+    
+    
+    
