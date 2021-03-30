6fe0f2789d6d08906b130c742e7063230c9f544a
enochadane <henokadane8@gmail.com>
2021-03-30 15:47:11 +0300

    bfs

---
+++ week-12 camp/jump_game_III.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        visited = set()
+        queue = deque()
+        queue.append(start)
+        visited.add(start)
+        while queue:
+            curr = queue.popleft()
+            if arr[curr] == 0:
+                return True
+            if curr + arr[curr] < len(arr) and curr + arr[curr] not in visited:
+                queue.append(curr + arr[curr])
+                visited.add(curr + arr[curr])
+            if curr - arr[curr] >= 0 and curr - arr[curr] not in visited:
+                queue.append(curr - arr[curr])
+                visited.add(curr - arr[curr])
+                
+        return False
