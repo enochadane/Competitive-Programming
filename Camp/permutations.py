df39da554a6e452e4b6b4acc4a0eb5b2bf92b24d
Henok <henokadane8@gmail.com>
2021-10-18 18:47:26 +0300

    permutation

--- Camp/permutations.py
+++ Camp/permutations.py
@@ -11,3 +11,19 @@ class Solution:
             result.extend(perms)
             nums.append(n)
         return result
+
+#second way
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        for n in nums:
+            self.recurse([n], nums, result)
+        return result
+    def recurse(self, arr, nums, result):
+        if len(arr) == len(nums):
+            result.append(arr)
+        for n in nums:
+            if n not in arr:
+                c_arr = arr[:]
+                c_arr.append(n)
+                self.recurse(c_arr, nums, result)
